6d86653c3ce1e186f97b11b1e76a260a361e5a6a
haok1402 <haok1402@gmail.com>
2025-03-04 01:34:11 -0500

    modify the training script for 469m to fit into the docker container

--- scripts/train_llama_469m_remoe.sh
+++ scripts/train_llama_469m_remoe.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
+pip install wandb
+
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-
-GPUS_PER_NODE=${1:-"1"}
-MASTER_ADDR=${MASTER_ADDR:-"localhost"}
-MASTER_PORT=${MASTER_PORT:-"6000"}
-NNODES=${SLURM_NNODES:-"1"}
-NODE_RANK=${RANK:-"0"}
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+export OMP_NUM_THREADS=8
 
 # 512 * 1k * 60k = 30b tokens.
 TRAIN_ITERS=${2:-"60000"}
@@ -17,75 +13,34 @@ NUM_EXPERTS=${4:-"8"}
 GRANILARITY=${5:-"1"}
 PROJECT_NAME=${6:-"test"}
 
-CHECKPOINT_PATH="./logs/$PROJECT_NAME"
+CHECKPOINT_PATH=/workspace/weights/$PROJECT_NAME
 mkdir -p $CHECKPOINT_PATH
 
 PILE_DATASET="\
 1.0 \
-../pile_gpt2/01_text_document \
+/workspace/dataset/pile_gpt2/00_text_document \
 1.0 \
-../pile_gpt2/02_text_document \
+/workspace/dataset/pile_gpt2/01_text_document \
 1.0 \
-../pile_gpt2/03_text_document \
+/workspace/dataset/pile_gpt2/02_text_document \
 1.0 \
-../pile_gpt2/04_text_document \
+/workspace/dataset/pile_gpt2/03_text_document \
 1.0 \
-../pile_gpt2/05_text_document \
+/workspace/dataset/pile_gpt2/04_text_document \
 1.0 \
-../pile_gpt2/06_text_document \
+/workspace/dataset/pile_gpt2/05_text_document \
 1.0 \
-../pile_gpt2/07_text_document \
+/workspace/dataset/pile_gpt2/06_text_document \
 1.0 \
-../pile_gpt2/08_text_document \
-1.0 \
-../pile_gpt2/09_text_document \
-1.0 \
-../pile_gpt2/10_text_document \
-1.0 \
-../pile_gpt2/11_text_document \
-1.0 \
-../pile_gpt2/12_text_document \
-1.0 \
-../pile_gpt2/13_text_document \
-1.0 \
-../pile_gpt2/14_text_document \
-1.0 \
-../pile_gpt2/15_text_document \
-1.0 \
-../pile_gpt2/16_text_document \
-1.0 \
-../pile_gpt2/17_text_document \
-1.0 \
-../pile_gpt2/18_text_document \
-1.0 \
-../pile_gpt2/19_text_document \
-1.0 \
-../pile_gpt2/20_text_document \
-1.0 \
-../pile_gpt2/21_text_document \
-1.0 \
-../pile_gpt2/22_text_document \
-1.0 \
-../pile_gpt2/23_text_document \
-1.0 \
-../pile_gpt2/24_text_document \
-1.0 \
-../pile_gpt2/25_text_document \
-1.0 \
-../pile_gpt2/26_text_document \
-1.0 \
-../pile_gpt2/27_text_document \
-1.0 \
-../pile_gpt2/28_text_document \
-1.0 \
-../pile_gpt2/29_text_document"
+/workspace/dataset/pile_gpt2/07_text_document"
 
 DISTRIBUTED_ARGS=(
-    --nproc_per_node $GPUS_PER_NODE
-    --nnodes $NNODES
-    --node_rank $NODE_RANK
-    --master_addr $MASTER_ADDR
-    --master_port $MASTER_PORT
+    --nnodes $SLURM_NNODES
+    --node_rank $SLURM_NODEID
+    --nproc_per_node $SLURM_GPUS_ON_NODE
+    --rdzv-id $SLURM_JOB_ID
+    --rdzv-backend c10d
+    --rdzv-endpoint $MASTER_ADDR:$MASTER_PORT
 )
 
 MODEL_ARGS=(
@@ -131,8 +86,8 @@ MOE_ARGS=(
 )
 
 DATA_ARGS=(
-    --vocab-file ../gpt2-vocab.json \
-    --merge-file ../gpt2-merges.txt \
+    --vocab-file /workspace/dataset/gpt2-vocab.json \
+    --merge-file /workspace/dataset/gpt2-merges.txt \
     --make-vocab-size-divisible-by 1024 \
     --data-path $PILE_DATASET
     --split 969,30,1
@@ -151,6 +106,7 @@ TRAINING_ARGS=(
 )
 
 MODEL_PARALLEL_ARGS=(
+    --no-gradient-accumulation-fusion # https://github.com/NVIDIA/Megatron-LM/issues/112
     --tensor-model-parallel-size 1
     --pipeline-model-parallel-size 1
     --expert-model-parallel-size 1
@@ -176,11 +132,11 @@ if [ -n "${WANDB_API_KEY}" ]; then
     )
 fi
 
-
+cd /workspace/ReMoE
 torchrun ${DISTRIBUTED_ARGS[@]} pretrain_gpt.py \
     ${MODEL_ARGS[@]} \
     ${MOE_ARGS[@]} \
     ${DATA_ARGS[@]} \
     ${TRAINING_ARGS[@]} \
     ${MODEL_PARALLEL_ARGS[@]} \
-    ${LOGGING_ARGS[@]} |& tee -a $CHECKPOINT_PATH/train.log
+    ${LOGGING_ARGS[@]}
